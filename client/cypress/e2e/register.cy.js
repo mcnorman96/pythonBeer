5746359f7260dc9d28d6e585e1f6435a2e672109
MarcusLaptop <marcus-norman@hotmail.com>
2025-10-26 15:16:09 +0100

    feat: implemented cypress with tests

---
+++ client/cypress/e2e/register.cy.js
@@ -0,0 +1,29 @@
+/* global describe, it, cy */
+describe('Login page', () => {
+  it('loads successfully', () => {
+    cy.visit('http://localhost:3000/register');
+    cy.contains('Register');
+    cy.contains('Username');
+    cy.contains('Password');
+    cy.contains('Email');
+  });
+
+  it('shows validation errors for empty fields', () => {
+    cy.visit('http://localhost:3000/register');
+    cy.wait(500);
+    cy.get('button[type="submit"]').click();
+    cy.wait(1500);
+    cy.contains('Username, Password and Email is required');
+  });
+
+  it('shows error for invalid credentials', () => {
+    cy.visit('http://localhost:3000/register');
+    cy.wait(500);
+    cy.get('input[name="username"]').type('user');
+    cy.get('input[name="password"]').type('pass');
+    cy.get('input[name="email"]').type('newone@gmail.com');
+    cy.get('button[type="submit"]').click();
+    cy.wait(500);
+    cy.contains('Registration successful! You can now log in.');
+  });
+});
